94bf07bcffdfbe16ca9033a920538d29983c9a94
Sanjeet Kumar <39269529+heysanjeet@users.noreply.github.com>
2026-03-29 21:55:40 +0530

    Add: buy-and-sell-crypto - submission-51

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-51.java
@@ -0,0 +1,16 @@
+class Solution {
+    //Cretae a veriable to store maxprofit
+    //iterate a price from i
+    public int maxProfit(int[] prices) {
+        int maxProfit=0;
+        for(int i=0;i<prices.length;i++){
+            for(int j=i+1; j<prices.length;j++){
+                if(prices[j]>prices[i]){
+                    maxProfit=Math.max(maxProfit, prices[j]-prices[i]);
+                }
+
+            }
+        }
+        return maxProfit;
+    }
+}
